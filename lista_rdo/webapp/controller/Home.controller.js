20b85df730560401534f22e3dff5a5365218a8b6
Lulle91 <125670291+Lulle91@users.noreply.github.com>
2024-02-02 17:47:01 +0100

    mod

--- lista_rdo/webapp/controller/Home.controller.js
+++ lista_rdo/webapp/controller/Home.controller.js
@@ -20,6 +20,43 @@ sap.ui.define(
 				let oModel= new JSONModel(data)
 				this.setModel(oModel,"richiesteModel");
       },
+      onOpenBom: function(oEvent){
+        debugger
+        let oDatiMat = this.getModel("richiesteModel").getData().bom[0]
+        let oModel = new JSONModel(oDatiMat)
+    
+        if(oEvent.getParameter("cellControl").getBindingInfo("text").binding.getPath() === 'Materiale'){
+          if(oEvent.getParameter("cellControl").getBindingInfo("text").binding.getValue() === oDatiMat.Materiale){
+            this.setModel(oModel, "bomDialog");
+            this.onOpenDialog("nDialog","listardo.listardo.view.Fragment.bomDialog",this,"bomDialog")
+          }
+        }
+      },
+      onCloseDialog: function(oEvent){
+        oEvent.getSource().getParent().close()
+      },
+      onUploadAllegati:function(oEvent){
+        debugger
+        
+         
+      },
+      onOpenAllegati: function (oEvent){
+        debugger
+        let oAllegatiMat = this.getModel("richiesteModel").getData().allegati[0]
+        let oModel = new JSONModel(oAllegatiMat)
+        let order = oEvent.getSource().getBindingContext("richiesteModel").getObject().richiesta
+        if(oAllegatiMat.Ordine === order){
+        this.setModel(oModel, "allegatiDialog");
+        this.onOpenDialog("pDialog","listardo.listardo.view.Fragment.listaAllegati",this,"allegatiDialog")     
+        }    
+      },
+      onSelectAllegato: function (oEvent){
+        debugger
+
+      },
+      onCloseAllegati: function (oEvent){
+        oEvent.getSource().getParent().getParent().close()
+      }
     });
   }
 );
